4740f38e2109e8e9855e5e8357c22bd8c4b08d39
pedroqdvieira <80059547+pedroqdvieira@users.noreply.github.com>
2024-06-12 19:16:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function Cadastro(event) {
+
+    event.preventDefault();
+
+    let email = document.getElementById('email');
+    let senha = document.getElementById('senha');
+    let alertDiv = document.getElementById('alert');
+    let message_type = document.getElementById('message_type');
+    let message = document.getElementById('message');
+
+
+    if (email.value !== '' && senha.value !== '') {
+
+        alert('Cadastro realizado com sucesso!');
+        window.location.reload();
+    } else {
+
+        if (email.value == '') {
+            message_type.innerText = 'Erro:';
+            message.innerText = 'Você precisa inserir seu email para realizar o cadastro.';
+            alertDiv.style.display = 'block';
+        } else if (senha.value == '') {
+            message_type.innerText = 'Erro:';
+            message.innerText = 'Você precisa inserir uma senha para realizar o cadastro.';
+            alertDiv.style.display = 'block';
+        }
+
+        setTimeout(() => {
+            closeMessage();
+        }, 4000);
+    }
+}
+
+function closeMessage() {
+    let alertDiv = document.getElementById('alert');
+    alertDiv.style.display = 'none';
+}

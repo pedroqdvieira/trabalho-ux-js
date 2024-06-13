18640dcaf6efafeeb3d4e82a3cf57598457d6626
Pedro Quinellato <80059547+pedroqdvieira@users.noreply.github.com>
2024-06-13 09:26:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 function Cadastro(event) {
-
     event.preventDefault();
 
     let email = document.getElementById('email');
     let senha = document.getElementById('senha');
+    let senha2 = document.getElementById('senha2'); // Adicionando o campo senha2
     let alertDiv = document.getElementById('alert');
     let message_type = document.getElementById('message_type');
     let message = document.getElementById('message');
 
-
-    if (email.value !== '' && senha.value !== '') {
-
+    if (email.value !== '' && senha.value !== '' && senha.value === senha2.value) {
         alert('Cadastro realizado com sucesso!');
         window.location.reload();
     } else {
-
         if (email.value == '') {
             message_type.innerText = 'Erro:';
             message.innerText = 'Você precisa inserir seu email para realizar o cadastro.';
@@ -22,6 +19,10 @@ function Cadastro(event) {
         } else if (senha.value == '') {
             message_type.innerText = 'Erro:';
             message.innerText = 'Você precisa inserir uma senha para realizar o cadastro.';
+            alertDiv.style.display = 'block';
+        } else if (senha.value !== senha2.value) {
+            message_type.innerText = 'Erro:';
+            message.innerText = 'As senhas não são iguais.';
             alertDiv.style.display = 'block';
         }
 
